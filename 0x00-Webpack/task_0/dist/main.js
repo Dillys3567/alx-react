5aec9f4ade970c4e90db5f96a19ad10f5eb7b45d
Dillys Annan <dillysannan@gmail.com>
2024-03-04 22:18:56 +0000

    webpack init

---
+++ 0x00-Webpack/task_0/dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("document.addEventListener(\"DOMContentLoaded\", function () {\r\n  var body = document.querySelector(\"body\");\r\n\r\n  var paragraph1 = document.createElement(\"p\");\r\n  paragraph1.textContent = \"Holberton Dashboard\";\r\n  body.appendChild(paragraph1);\r\n\r\n  var paragraph2 = document.createElement(\"p\");\r\n  paragraph2.textContent = \"Dashboard data for the students\";\r\n  body.appendChild(paragraph2);\r\n\r\n  var paragraph3 = document.createElement(\"p\");\r\n  paragraph3.textContent = \"Copyright - Holberton School\";\r\n  body.appendChild(paragraph3);\r\n});\r\n\n\n//# sourceURL=webpack://task_0/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,15 @@
-import $ from "jquery";
+document.addEventListener("DOMContentLoaded", function () {
+  var body = document.querySelector("body");
 
-$("body").append(<p>Holberton Dashboard</p>);
-$("body").append(<p>Dashboard data for the students</p>);
-$("body").append(<p>Copyright - Holberton School</p>);
+  var paragraph1 = document.createElement("p");
+  paragraph1.textContent = "Holberton Dashboard";
+  body.appendChild(paragraph1);
+
+  var paragraph2 = document.createElement("p");
+  paragraph2.textContent = "Dashboard data for the students";
+  body.appendChild(paragraph2);
+
+  var paragraph3 = document.createElement("p");
+  paragraph3.textContent = "Copyright - Holberton School";
+  body.appendChild(paragraph3);
+});
